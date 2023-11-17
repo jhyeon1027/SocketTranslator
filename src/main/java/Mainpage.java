7977c7898457e15245d4e5d477e67457a39d32c7
guri172 <guri172172@gmail.com>
2023-11-17 20:47:26 +0900

    3번안 커밋푸시

--- src/main/java/Mainpage.java
+++ src/main/java/Mainpage.java
@@ -17,26 +17,81 @@ public class Mainpage {
 
     class RoundedButton extends JButton {
 
+        private Color bgColor;
+        public RoundedButton(String text, Color bgColor){
+            super(text);
+            this.bgColor = bgColor;
+            decorate();
+        }
+
         public RoundedButton() {
             super();
+            decorate();
         }
 
         public RoundedButton(String text) {
             super(text);
+            decorate();
         }
 
         public RoundedButton(Action action) {
             super(action);
+            decorate();
         }
 
         public RoundedButton(Icon icon) {
             super(icon);
+            decorate();
         }
 
         public RoundedButton(String text, Icon icon) {
             super(text, icon);
+            decorate();
         }
-    }
+
+        protected void decorate() {
+            setBorderPainted(false);
+            setOpaque(false);
+            setBackground(bgColor);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            int width = getWidth();
+            int height = getHeight();
+
+            Graphics2D graphics = (Graphics2D) g;
+
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            if (getModel().isArmed()) {
+                graphics.setColor(getBackground().darker());
+            } else if (getModel().isRollover()) {
+                graphics.setColor(getBackground().brighter());
+            } else {
+                graphics.setColor(getBackground());
+            }
+
+            if (this.getText().equals("나가기")){ // b5 버튼의 둥근 정도를 작게 조정
+                graphics.fillRoundRect(0, 0, width, height, 0, 0); // b5 버튼만 둥근 정도를 줄임
+            } else {
+                graphics.fillRoundRect(0, 0, width, height, 0, 0); // 다른 버튼들은 기존 둥근 정도 유지
+            }
+
+            FontMetrics fontMetrics = graphics.getFontMetrics();
+            Rectangle stringBounds = fontMetrics.getStringBounds(this.getText(), graphics).getBounds();
+
+            int textX = (width - stringBounds.width) / 2;
+            int textY = (height - stringBounds.height) / 2 + fontMetrics.getAscent();
+
+            graphics.setColor(getForeground());
+            graphics.setFont(getFont());
+            graphics.drawString(getText(), textX, textY);
+            graphics.dispose();
+
+            super.paintComponent(g);
+        }
+    }     // 버튼 둥글게 변경하는 클래스
 
     public void openMainpage() {
         JFrame f = new JFrame("CATPAGO - 언어 장벽 없이 대화하는 세상을 꿈꿉니다. "); // JFrame 객체를 생성한다.
@@ -65,18 +120,18 @@ public class Mainpage {
 
 
         f.setResizable(false); // 윈도우의 크기 조정을 불가능하게 한다.
-        JButton b1 = new JButton("텍스트 번역"); // JButton 객체를 생성한다.
-        JButton b2 = new JButton("이미지 번역"); // JButton 객체를 생성한다.
-        JButton b3 = new JButton("PDF   번역"); // JButton 객체를 생성한다.
-        JButton b4 = new JButton("글로벌 채팅"); // JButton 객체를 생성한다.
-        JButton b5 = new JButton("나가기"); // JButton 객체를 생성한다.
+        RoundedButton b1 = new RoundedButton("텍스트 번역",new Color(131,162,255)); // JButton 객체를 생성한다.
+        RoundedButton b2 = new RoundedButton("이미지 번역",new Color(180,189,255)); // JButton 객체를 생성한다.
+        RoundedButton b3 = new RoundedButton(" PDF  번역",new Color(255,227,187)); // JButton 객체를 생성한다.
+        RoundedButton b4 = new RoundedButton("글로벌 채팅",new Color(255,210,143)); // JButton 객체를 생성한다.
+        RoundedButton b5 = new RoundedButton("나가기", Color.LIGHT_GRAY); // JButton 객체를 생성한다.
 
 
-        b1.setBounds(148, 550, 200, 50); // 버튼의 위치와 크기를 설정한다.
-        b2.setBounds(148, 620, 200, 50); // 버튼의 위치와 크기를 설정한다.
-        b3.setBounds(148, 690, 200, 50); // 버튼의 위치와 크기를 설정한다.
-        b4.setBounds(148, 760, 200, 50); // 버튼의 위치와 크기를 설정한다.
-        b5.setBounds(375, 850, 100, 30); // 버튼의 위치와 크기를 설정한다.
+        b1.setBounds(0, 450, 500, 100); // 버튼의 위치와 크기를 설정한다.
+        b2.setBounds(0, 550, 500, 100); // 버튼의 위치와 크기를 설정한다.
+        b3.setBounds(0, 650, 500, 100); // 버튼의 위치와 크기를 설정한다.
+        b4.setBounds(0, 750, 500, 100); // 버튼의 위치와 크기를 설정한다.
+        b5.setBounds(0, 850, 500, 40); // 버튼의 위치와 크기를 설정한다.
 
         f.add(b1); // 버튼을 윈도우에 추가한다.
         f.add(b2); // 버튼을 윈도우에 추가한다.
